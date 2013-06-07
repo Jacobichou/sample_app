ed1820b0fe7d826b260c3abbbd7f8bcbd4b2ea99
Jacob Pagano <jacob@Pagano>
2013-06-07 14:43:55 -0500

    Password exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -65,6 +65,16 @@ describe User do
     end
   end
 
+  describe "email with mixed case" do
+    let(:mixed_case_email) { "Foo@exAMPle.CoM" }
+
+    it "should be saved in all lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
