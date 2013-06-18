59fede7aa6b7d463fe7ea1b2524adcef61bbfd7f
Jacob Pagano <jacob@Pagano>
2013-06-18 10:59:07 -0500

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def create
+  	@user = User.new(params[:user])
+
+  	if @user.save
+  		# Handle successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
 end
