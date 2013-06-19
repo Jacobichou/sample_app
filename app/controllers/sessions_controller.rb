b92728e99fefbddaaf2eb287561e1fab94feb671
Jacob Pagano <jacob@Pagano>
2013-06-18 21:41:17 -0500

    Refactored login form to use form_tag

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,21 +4,30 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
-			# sign user in
+		# user = User.find_by_email(params[:session][:email].downcase)
+		# if user && user.authenticate(params[:session][:password])
+		# 	# sign user in
+		# 	sign_in user
+		# 	redirect_to user
+		# else
+		# 	# create error msg
+		# 	flash.now[:error] = 'Invalid email/password combination'
+		# 	render 'new'
+		# end
+
+		user = User.find_by_email(params[:email].downcase)
+		if user && user.authenticate(params[:password])
 			sign_in user
-			redirect_to user
+			redirect_to user, notice: "Welcome, #{user.name}."
 		else
-			# create error msg
-			flash.now[:error] = 'Invalid email/password combination'
+			flash.now[:error] = "Invalid email or password"
 			render 'new'
 		end
 	end
 
 	def destroy
 		sign_out
-		redirect_to root_url
+		redirect_to root_url, notice: "Successfully logged out. Good bye!"
 	end
 
 end
