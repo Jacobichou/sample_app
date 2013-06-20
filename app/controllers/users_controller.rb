89211b6f7c4395a26da20de968b8b0af5c8679b8
Jacob Pagano <jacob@Pagano>
2013-06-19 23:28:01 -0500

    Added authorization tests and edit & update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user, only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -19,4 +22,28 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(params[:user])
+      # handle successful update
+      flash[:success] = "Profile updated!"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
 end
