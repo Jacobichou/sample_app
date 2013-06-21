8a8646a4a7864f1c3a371426b6f8679340f49856
Jacob Pagano <jacob@Pagano>
2013-06-20 20:55:18 -0500

    Finished User resource

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -97,5 +97,18 @@ describe "Authentication" do
 				specify { response.should redirect_to(root_path) }
 			end
 		end
+
+		describe "as non-admin user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:non_admin) { FactoryGirl.create(:user) }
+
+			before { sign_in non_admin }
+
+			describe "Submitting a DELETE req to the Users#destroy action" do
+				before { delete user_path(user) }
+				specify { response.should redirect_to(root_path) }
+			end
+		end
+
 	end
 end
